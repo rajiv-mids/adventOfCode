32389887d953d7180b705ca8af00a4cfc5c1275e
Rajiv Nair <rajiv.nair@berkeley.edu>
2020-12-01 06:24:35 -0500

    day1

---
+++ 01/day1.py
@@ -0,0 +1,26 @@
+#!/home/rajivn/anaconda3/bin/python
+from collections import defaultdict
+nums = defaultdict(list)
+nums_orig = list()
+with open("/home/rajivn/adventOfCode/01/data", "r") as input:
+    for i, v in enumerate(input.readlines()):
+        nums[int(v)].append(int(i))
+        nums_orig.append(int(v))
+
+
+def lookfor(nums_map, nums_orig, num, skip):
+    # look for number
+    for k, v in enumerate(nums_orig):
+        look = num-v
+        if look in nums:
+            arr =  nums[look]
+            for i, l in enumerate(arr):
+                if i != k and i != skip:
+                    return (v , look)
+    return None, None
+
+for i, v in enumerate(nums_orig):
+    lf = 2020-v
+    n2, n3 = lookfor(nums, nums_orig, lf, i)
+    if n2 is not None:
+        print(v * n2 * n3)
