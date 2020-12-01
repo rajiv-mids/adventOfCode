5814bfaa4ab7d7dcfd97d29506fac23f7d15c225
Rajiv Nair <rajiv.nair@berkeley.edu>
2020-12-01 09:39:25 -0500

    day1

--- 01/day1.py
+++ 01/day1.py
@@ -19,8 +19,16 @@ def lookfor(nums_map, nums_orig, num, skip):
                     return (v , look)
     return None, None
 
+#part 1
+for i, v in enumerate(nums_orig):
+    n2, n3 = lookfor(nums, nums_orig, 2020, i)
+    if n2 is not None:
+        print( n2 * n3)
+        break
+#part 2
 for i, v in enumerate(nums_orig):
     lf = 2020-v
     n2, n3 = lookfor(nums, nums_orig, lf, i)
     if n2 is not None:
         print(v * n2 * n3)
+        break
