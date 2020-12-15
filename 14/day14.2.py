7358814d4849935083c705474507f5a5985004fe
Rajiv Nair <rajiv.nair@berkeley.edu>
2020-12-14 19:53:54 -0500

    day14

--- 14/day14.2.py
+++ 14/day14.2.py
@@ -4,9 +4,6 @@ import re
 import itertools
 mem = defaultdict(int)
 mask = None
-mems = dict()
-replaces = dict()
-replaced_by = dict()
 memvals = dict()
 
 def do_mask_1(val, mask):
@@ -20,41 +17,16 @@ def do_mask_1(val, mask):
         b_val = b_val[:i]+"X"+b_val[i+1:]
     return b_val
 
-def find_msb(maskstr):
-    x_id = maskstr.index("X")
-    m_id = maskstr.index("1")
-    return x_id if x_id < m_id else m_id
-
-def sub_overlaps(l_mask1):
-    sub = 0
-    for k, v in mems.items():
-        n1, n2 = find_msb(l_mask1), find_msb(k)
-        rang = n1 if n1 < n2 else n2
-        for a1, b1 in zip([x for x in l_mask1[rang:]], [x for x in k[rang:]] ):
-            if a1 != "X" and b1 != "X" and a1!= b1:
-                break
-        else:
-            combos_a = pow(2, len([m.start() for m in re.finditer('X', k)]) )
-            tot_matches = combos_a
-            for a, b in zip(k[rang:], l_mask1[rang:]):
-                if a != 'X' and b!= 'X':
-                    continue
-                if a == "X" and b != "X" :
-                    tot_matches = int(tot_matches/2)
-            sub += (tot_matches * v)
-    return sub
 
 actuals = {}
 
 def apply(mask_1, val):
     floats = [m.start() for m in re.finditer('X', mask)]
     c = itertools.product("01", repeat=len(floats))
-    print("====")
     for combo in c:
         key = mask_1
         for l, v in zip(combo, floats):
             key = key[:v]+l+key[v+1:]
-        print("mask", mask_1, "key", key)
         memvals[key] = val
 
 with open("/home/rajivn/adventOfCode/14/input", "r") as inp:
@@ -73,13 +45,6 @@ with open("/home/rajivn/adventOfCode/14/input", "r") as inp:
             floats = [m.start() for m in re.finditer('X', l_mask1)]
             cur_tot = pow(2, len(floats)) * val
             
-            # go through prior vals and subtract
-            remove = sub_overlaps(l_mask1)
-            mems[l_mask1] = val
-            tot += cur_tot
-            tot -= remove
-
-            #testing code
             apply(l_mask1, val)
 
     print("part 2", tot)
